54497c7084a3c5c02d5a19857ed57eac60be5765
GardenOfOctopus <s02190702@gse.cs.msu.ru>
2024-05-25 01:34:05 +0300

    TelegramBot: create first implementation

--- TelegramBot.py
+++ TelegramBot.py
@@ -1,4 +1,37 @@
 from BotBase import BotBase
+import asyncio
+import aiogram
 
 class TelegramBot(BotBase):
-    pass
+    
+    def __init__(self, parameters):
+        self.token = parameters['token']
+        self.chat_id = parameters["channel"]
+
+        self.bot = aiogram.Bot(token=self.token)
+        self.dp = aiogram.Dispatcher()
+
+        @self.dp.message()
+        async def on_message(message):
+            self.base_message_handler(message)
+
+        print("Created TelegramBot")
+
+    async def start(self):
+        
+        self.bot_user = await self.bot.get_me()
+        await self.dp.start_polling(self.bot)
+
+    def base_message_handler(self, message):
+        print("Id чата:", message.from_user.id)
+        if message.from_user == self.bot_user:
+            return
+        self.outcoming.put_nowait(message)
+
+    async def send(self, message):
+        text = message
+        await self.bot.send_message(self.chat_id, text) 
+
+
+
+        
