8b257d6be44e3d4787048599c05b9f9612353a60
GardenOfOctopus <s02190702@gse.cs.msu.ru>
2024-05-26 15:29:33 +0300

    VkBot: add first implementation

--- VkBot.py
+++ VkBot.py
@@ -1,4 +1,46 @@
 from BotBase import BotBase
+from vkbottle.bot import Bot
+from vkbottle import API
+
+from Message import Message, Attachment
+
+import datetime
 
 class VkBot(BotBase):
-    pass
+        
+    def __init__(self, parameters):
+        self.token = parameters['token']
+        self.peer_id = parameters["channel"]
+
+        print(self.token)
+        self.api = API(token=self.token)
+        self.bot = Bot(api=self.api)
+
+
+        @self.bot.on.message()
+        async def on_message(msg):
+            self.base_message_handler(msg)
+
+        print("Created VkBot")
+
+    async def start(self):
+        await self.bot.run_polling()
+
+    def base_message_handler(self, msg):
+        print("Chat ID:", msg.chat_id)
+        print("Peer ID:", msg.peer_id)
+        print("From Id:", msg.from_id)
+        if msg.peer_id != self.peer_id:
+            return
+
+        message = Message(str(msg.from_id),
+              datetime.date(1, 1, 1),
+              text=msg.text,
+              attachments=[])
+        
+        message.source = "vk"
+        
+        self.outcoming.put_nowait(message)
+
+    async def send(self, message):
+        await self.api.messages.send(random_id = 0, peer_id=self.peer_id, message=message.text) 
