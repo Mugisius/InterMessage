252f082a7ec87e7f14162a2510636f16940ad36b
GardenOfOctopus <s02190702@gse.cs.msu.ru>
2024-06-03 02:22:32 +0300

    tests: add TestAttachment

---
+++ tests/TestAttachment.py
@@ -0,0 +1,34 @@
+import unittest
+import yaml
+import os
+import sys
+
+sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
+from InterMessage.Message import Attachment
+
+class TestCreateNodes(unittest.TestCase):
+
+    def test_a_only_type(self):
+        a = Attachment("image/jpeg")
+        self.assertIsNone(a.file)
+        self.assertEqual(a.filename, "image.jpeg")
+
+    def test_a_by_url(self):
+        a = Attachment("image/jpeg", url="https://google.com")
+        self.assertIsNotNone(a.file)
+        self.assertEqual(a.filename, "image.jpeg")
+
+    def test_a_by_file(self):
+        file = 5
+        a = Attachment("image/jpeg", file=file)
+        self.assertEqual(a.file, file)
+        self.assertEqual(a.filename, "image.jpeg")
+
+    def test_a_filename(self):
+        filename="filename"
+        a = Attachment("image/jpeg", filename=filename)
+        self.assertIsNone(a.file)
+        self.assertEqual(a.filename, filename)
+       
+if __name__ == "__main__":
+    unittest.main()
